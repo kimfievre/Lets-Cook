becfcbfc16d5d415d28f84414a728202ae92d798
Kimberly Fievre <kimfievre@users.noreply.github.com>
2016-09-27 23:31:21 -0500

    updated files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('.addBtn').click(function(){
         var toAdd = $('input[name=checkListItem]').val();
-        // var yummlyAPI = "http://api.yummly.com/v1/api/recipes?_app_id=e7570b5b&_app_key=ec1e10dd3a2667dd452af19b8b1682d3&q=";
+        $(".list").append('<div class="item">' + toAdd + '</div>');
         var API = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?&limitLicense=false&number=5&ranking=1&fillIngredients=true&ingredients=";
         $.getJSON(API + toAdd + API_KEY, function (data) {
         	var $showRecipe = $("#showRecipe");
